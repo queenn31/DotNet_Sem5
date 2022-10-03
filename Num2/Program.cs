0a57df3e52f2f1b1116db42916441c36f6d5a3af
queenn31 <queenn31@gmail.com>
2022-10-03 19:49:00 +0300

    added Num2

---
+++ Num2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write($"Введи количество элементов: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Итог: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+Console.WriteLine($"\nСумма нечётных: {randomNumbers}");
